9c4f7b248d9494a8562256221054b0ff8f94ea9c
Pablo01bm <pablobormeg@gmail.com>
2022-10-18 20:05:43 +0200

    [FAN-44] Login page view finished

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,6 +1,9 @@
+import 'package:fanana/Pages/admin/landingPageAdmins.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:email_validator/email_validator.dart';
 
 class Login  extends StatefulWidget {
 
@@ -17,12 +20,34 @@ class Login  extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  late MediaQueryData queryData;
+  final emailController = TextEditingController();
 
-  //late String nombreeee;
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    emailController.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  void initState() {
+    
+    super.initState();
+
+    emailController.addListener(() {
+      setState(() {
+        
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    
+    queryData = MediaQuery.of(context);
 
 
     return Scaffold(
@@ -45,19 +70,116 @@ class _LoginState extends State<Login> {
             endIndent: 100,
             color: Color.fromARGB(157, 132, 74, 74),
           ),
-        loginDefault(),
+        loginPicto(),
       ],
     );
   }
 
   Widget loginDefault(){
+    return Form(
+      child:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          FittedBox(
+            fit: BoxFit.fill,
+            child: Text("Login", style: GoogleFonts.fredokaOne(
+              textStyle: TextStyle(fontSize: queryData.size.width*0.04)
+            )),      
+          ),
+          SizedBox(        
+            width: queryData.size.width * 0.4,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Introduzca email:",
+                labelStyle: GoogleFonts.fredokaOne(textStyle: TextStyle(fontSize: queryData.size.width*0.03)), 
+              ),
+              autovalidateMode: AutovalidateMode.always,
+              validator: (value) => EmailValidator.validate(value!) ? null:"Introduzca email válido"
+            ), 
+          ),
+
+          SizedBox(
+            width: queryData.size.width * 0.4,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Introduzca contraseña:",
+                labelStyle: GoogleFonts.fredokaOne(textStyle: TextStyle(fontSize: queryData.size.width*0.03)), 
+              ),
+            ), 
+          ),
+          FittedBox(
+          fit: BoxFit.fill,
+          child: InkWell(   
+            onTap: () {
+              
+            },
+            child: TextButton(
+              child: Image(
+                fit: BoxFit.fill,
+                //height: queryData.size.height * 0.7,
+                width: queryData.size.width * 0.15,
+                image: const AssetImage("botonfumon.png")
+                ),
+              onPressed: () {
+                 Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const landingPageAdmin()),
+                );
+              },
+            ),
+          ),
+        )
+          
+
+        ],
+      ),
+
+    );
+  }
+
+   Widget loginPicto(){
     return Column(
-      children: const <Widget>[
-        Text("Login", style: TextStyle(
-          fontSize: 20,
-        ),)
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        FittedBox(
+          
+          fit: BoxFit.fill,
+          child: Text("Login pictograma", style: GoogleFonts.fredokaOne(
+            textStyle:  TextStyle(fontSize: queryData.size.width*0.04)
+          )),      
+        ),
+        FittedBox(
+          fit: BoxFit.fill,
+          child: InkWell(   
+            onTap: () {
+              
+            },
+            child: TextButton(
+              child: Image(
+                fit: BoxFit.fill,
+                //height: queryData.size.height * 0.7,
+                width: queryData.size.width * 0.4,
+                image: const AssetImage("nino.png")
+                ),
+              onPressed: () {},
+            ),
+          ),
+        )
+
       ],
     );
+  }
+  
+
+String? validateEmail(String? value) {
+    String pattern =
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+.[a-zA-Z]+";
+    RegExp regex = RegExp(pattern);
+    if (value == null || value.isEmpty || !regex.hasMatch(value))
+      return 'Introduce un email válido';
+    else
+      return null;
   }
 
 
